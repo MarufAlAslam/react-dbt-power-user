8ecb140cfc37df3e0c110d60484d8d6ef0f12d4a
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-28 21:35:10 +0600

    homepage added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./assets/css/global.css";
+import Home from "./pages/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render("hello");
+root.render(<Home />);
