0587d83d42bb4d87af6d0b2dffbf0bf1f3e3d30c
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-08-02 00:43:14 +0600

    form responsive done

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -6,14 +6,27 @@ const Features = () => {
       <div className="custom-container">
         <div className="text-center">
           <h2 className="md:text-[40px] text-2xl font-bold text-white mb-[56px]">
-            Hear first about our upcoming <br className="md:hidden block" /> new features!!
+            Hear first about our upcoming <br className="md:block hidden" /> new
+            features!!
           </h2>
 
-          <form action="" className="w-1/3 mx-auto bg-white p-[3px] rounded-full overflow-hidden flex justify-between items-center">
-            <input type="email" placeholder="Email Address" className="px-[22px] py-[12px] text-black outline-none w-full placeholder:text-[#8390A3] bg-white h-full border-0" name="" id="" />
+          <form
+            action=""
+            className="md:w-1/3 w-full mx-auto bg-white p-[3px] rounded-full overflow-hidden flex justify-between items-center"
+          >
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="px-[22px] py-[12px] text-black outline-none w-full placeholder:text-[#8390A3] bg-white h-full border-0"
+              name=""
+              id=""
+            />
 
-            <button type="submit" className="rounded-full bg-[#11D588] text-[#082247] text-lg font-[500] h-full px-[32px] py-[12px]">
-                Send
+            <button
+              type="submit"
+              className="rounded-full bg-[#11D588] text-[#082247] text-lg font-[500] h-full px-[32px] py-[12px]"
+            >
+              Send
             </button>
           </form>
         </div>
